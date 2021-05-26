060db5abb96684bd92df3c5ee5cc7a0a8324ea68
doowoncho <84430881+doowoncho@users.noreply.github.com>
2021-05-26 01:10:36 -0600

    Nice and pretty

--- src/codes/Dealer.java
+++ src/codes/Dealer.java
@@ -7,72 +7,96 @@ import java.util.concurrent.TimeUnit;
 public class Dealer {
 	
 	static int handValue = 0;
-	
-	static int sum(ArrayList<Integer> list)  //method that returns the sum of the hand
-	  {
-	   int sum = 0;
-	   for ( Integer d:list)
-	     sum = sum + d;
-	    return sum;
-	  }
+	static int x = 0;
 	
 	public static void main(String []args) throws Exception{
 		
         Deck firstDeck = new Deck(1);   
         ArrayList<Card> drawnCards = new ArrayList<Card>();
-                
-        //dealer logic i guess
-        //added a delay between outputs just to be dramatic
-        int x = 0;
-        boolean hitting = true;
-		while (hitting == true){
-			
-			System.out.println("dealer hit"+ "\n");
-			TimeUnit.SECONDS.sleep(1);
-        	drawnCards.add(firstDeck.drawCard());
+    	
+		Hit(drawnCards, firstDeck, x);
+		
+      }
+		
+	/**
+	 * This method "hits" so it will draw a card from the deck array and will add that card to the drawnCards array. As well as call on the checkLogic function to see what to do next. 
+	 * @param drawnCards is an array that holds all the cards that have been drawn from the deck.
+	 * @param firstDeck is what the deck that is being used is called. I think Daniel made it like this so we can use multiple decks in the future.
+	 * @param x is what card in the drawnCards array is being checked/added to the total. (Basically keeping track of which card is added the most recently to the hand)
+	 */
+	public static void Hit(ArrayList<Card> drawnCards, Deck firstDeck, int x) {
+	
+		System.out.println("dealer hit"+ "\n");
+		drawnCards.add((Card) firstDeck.drawCard());
+		
+		System.out.println(drawnCards.get(x));
+		
+    	if(drawnCards.get(x).getValue().get(0) == 1 && handValue <= 10){
+    		handValue += drawnCards.get(x).getValue().get(1);
+    		
+    	}
+    	else
+    	{
+    		handValue += drawnCards.get(x).getValue().get(0);
+    	}
+	
+        System.out.println("Total is " + handValue + "\n");
+		x  += 1;
+		
+		checkLogic(drawnCards, firstDeck, x);
+		
 
-        	Card card = drawnCards.get(x);      	
-            System.out.println(drawnCards.get(x));     
-        	x += 1;        	
-        	
-        	if(card.getValue().get(0) == 1 && handValue <= 10){
-        		System.out.println("this is an ace");
-        		handValue += card.getValue().get(1);
-        		
-        	}
-        	else{
-        		handValue += card.getValue().get(0);
-        	}
 
-            if(handValue > 17) {
-            	hitting = false;
-            }
-
-            System.out.println("Total is " + handValue + "\n");
-            TimeUnit.SECONDS.sleep(2);
-        }
+	}
+	
+	/**
+	 * This method checks the value that is in the dealer's hand and see's if it should hit or hold. Also tells it if it won or not.
+	 * @param drawnCards
+	 * @param firstDeck
+	 * @param x
+	 */
+	public static void checkLogic(ArrayList<Card> drawnCards, Deck firstDeck, int x)
+	{
 		if (handValue > 21){
 			Lost();
 
 		}
-		if (handValue == 21){
+		else if (handValue == 21){
 			
 			Win();
 		}
-		if (handValue <21)
+		else if (handValue <17)
 		{
+			Hit(drawnCards, firstDeck, x);
+		}
+		
+		else if(handValue >= 17 && handValue <21) {
 			Hold();
 		}
-       }
-		
+		else
+		{
+			
+		}
+      
+	}
+
+	/** 
+	 * this method is to declare when the dealer wins
+	 */
 	public static void Win (){
-		System.out.println("Dealer Won");
+		System.out.println("Dealer Wins");
 	}
 	
+	/** 
+	 * this method is to declare when the dealer busts
+	 */
 	public static void Lost (){
-		System.out.println("fuck");
+		System.out.println("Dealer Loses");
 	}
 	
+	/** 
+	 * this method is to declare when the dealer holds
+	 */
 	public static void Hold (){
 		System.out.println("Dealer holds");
 	} 
