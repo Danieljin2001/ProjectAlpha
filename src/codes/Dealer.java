17171f8aa06e5c804c6b41b68520eb5aff203b8c
doowoncho <84430881+doowoncho@users.noreply.github.com>
2021-05-29 02:32:48 -0600

    Easier to mesh with the other classes

--- src/codes/Dealer.java
+++ src/codes/Dealer.java
@@ -4,36 +4,24 @@ import java.util.ArrayList;
 
 public class Dealer {
 	
-	static int handValue = 0;
-	static int x = 0;
-
-	private static ArrayList<Card> dealerHand = new ArrayList<Card>();
+	private Deck DECK;
+	private  ArrayList<Card> dealerHand = new ArrayList<Card>();
 	
-
-    
-	public static void main(String []args) throws Exception{
-		
-
-
-
-		
-      }
-		
-	/**
-	 * This method "hits" so it will draw a card from the deck array and will add that card to the drawnCards array. As well as call on the checkLogic function to see what to do next. 
-	 *  @param dealer cards is an array that holds all the cards that have been drawn and added to the dealers hand.
-	 * @param firstDeck is what the deck that is being used is called. I think Daniel made it like this so we can use multiple decks in the future.
-	 * @param x is what card in the drawnCards array is being checked/added to the total. (Basically keeping track of which card is added the most recently to the hand)
-	 */
-	public static void Hit(ArrayList<Card> drawnCards, Deck firstDeck, int x) {
+	private int handValue = 0;
+	
+	public  Dealer(Deck deck)
+	{
+		this.DECK = deck;
+	}
+	
+	
+	public  void Hit() {
 		System.out.println("dealer hit"+ "\n");
-		drawnCards.add((Card) firstDeck.drawCard());
-		dealerHand.add((Card) firstDeck.drawCard());
-		
-		
-		System.out.println(drawnCards.get(x));
 
+		dealerHand.add(DECK.drawCard());
+		int x = dealerHand.size() - 1;
 		
+		System.out.println(dealerHand);
     	if(dealerHand.get(x).getValue().get(0) == 1 && handValue <= 10){
     		handValue += dealerHand.get(x).getValue().get(1);
     		
@@ -44,20 +32,13 @@ public class Dealer {
     	}
 	
         System.out.println("Total is " + handValue + "\n");
-		x  += 1;
 
-		checkLogic(dealerHand, firstDeck, x);
+		checkLogic();
 		
 
 	}
 	
-	/**
-	 * This method checks the value that is in the dealer's hand and see's if it should hit or hold. Also tells it if it won or not.
-	 * @param dealer cards is an array that holds all the cards that have been drawn and added to the dealers hand.
-	 * @param firstDeck is what the deck that is being used is called. I think Daniel made it like this so we can use multiple decks in the future.
-	 * @param x is what card in the drawnCards array is being checked/added to the total. (Basically keeping track of which card is added the most recently to the hand)
-	 */
-	public static void checkLogic(ArrayList<Card> dealerHand, Deck firstDeck, int x)
+	public  void checkLogic()
 	{
 		
 		if (handValue > 21){
@@ -70,7 +51,7 @@ public class Dealer {
 		}
 		else if (handValue <17)
 		{
-			Hit(dealerHand, firstDeck, x);
+			Hit();
 		}
 		
 		else if(handValue >= 17 && handValue <21) {
@@ -86,22 +67,23 @@ public class Dealer {
 	/** 
 	 * this method is to declare when the dealer wins
 	 */
-	public static void Win (){
+	public  void Win (){
 		System.out.println("Dealer Wins");
 	}
 	
 	/** 
 	 * this method is to declare when the dealer busts
 	 */
-	public static void Lost (){
+	public  void Lost (){
 		System.out.println("Dealer Loses");
 	}
 	
 	/** 
 	 * this method is to declare when the dealer holds
 	 */
-	public static void Hold (){
+	public  void Hold (){
 		System.out.println("Dealer holds");
 	} 
+	
 	
 }
--- src/codes/Test.java
+++ src/codes/Test.java
@@ -5,9 +5,8 @@ import java.util.ArrayList;
 public class Test {
 	public static void main(String []args) throws Exception{
 	    Deck firstDeck = new Deck(1);   
-	    ArrayList<Card> drawnCards = new ArrayList<Card>();
-		
-		//Hit(drawnCards, firstDeck, x);
+		Dealer dealer  = new Dealer(firstDeck);
+		dealer.Hit();
        
 	}
 } 
